cd39c4da36dab7a2990d09910e657180ce8f2aff
maruncho <legomarkata@gmail.com>
2025-04-26 15:06:50 +0300

    Cleanup

--- bin/env.ml
+++ bin/env.ml
@@ -10,6 +10,3 @@ let base_tenv = Symbol.enter base_tenv (Symbol.symbol "int") Types.INT
 let base_tenv = Symbol.enter base_tenv (Symbol.symbol "string") Types.STRING
 
 let base_venv : enventry Symbol.table = Symbol.empty (* predefined functions *)
-
-(* Used mainly for array size sharing. Maybe ugly, but requires less refactoring.*)
-let var_sizes : Translate.exp Symbol.table ref = ref Symbol.empty
